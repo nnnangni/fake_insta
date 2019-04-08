3ec78c7a0a6de646a9abcc6547f14295047c5cb0
park nawon <yud02150@gmail.com>
2019-04-08 02:59:06 +0000

    django-bootstrap4 적용

--- posts/views.py
+++ posts/views.py
@@ -9,7 +9,7 @@ def create(request):
     # 1. get방식으로 데이터를 입력할 form을 요청한다.
     # 4. 사용자가 데이터를 입력해서 post방식으로 요청한다.
     # 9. 사용자가 다시 적절한 데이터를 입력해서 post방식으로 요청한다.
-    if request.method == "POST":
+    if request.method == "POST": # 저장
         # 5. 10. post방식으로 저장요청을 받고, 데이터를 받아 PostForm에 넣어서 인스턴스화 한다.
         form = PostForm(request.POST)
         # 6. 11. 데이터 검증을 한다.
@@ -20,7 +20,7 @@ def create(request):
         else:
             # 7. 적절하지 않은 데이터가 들어오는 경우
             pass
-    else:
+    else: # get방식이면 폼을 보여줌
         # 2. PostForm을 인스턴스화 시켜서 form에 저장한다.
         form = PostForm()
     # 3. form을 담아서 create.html을 보내준다.
