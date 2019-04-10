8f8f3c4f7d59b1a4ee10234f1761d310541cbd7e
park nawon <yud02150@gmail.com>
2019-04-10 04:37:57 +0000

    로그인 인증 추가

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
 # Create your views here.
 def signup(request):
@@ -11,5 +13,18 @@ def signup(request):
     else:
         form = UserCreationForm()
     # 왜 return이 바깥쪽에 있을까?
-    return render(request, 'accounts/signup.html',{'form':form})
+    return render(request, 'accounts/form.html',{'form':form})
     
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect("posts:list")
+    else:
+        form = AuthenticationForm()
+    return render(request,"accounts/form.html",{'form':form})
+
+def logout(request):
+    auth_logout(request)
+    return redirect("posts:list")
